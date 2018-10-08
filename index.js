7a51e7fbcbfba0070709bdaa506e522e5034960f
Mr. India <mr.india.dev@gmail.com>
2018-10-08 10:30:38 +0000

    Added Banner and Category

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 angular
-.module('100gmInfo', [])
+.module('localmarket', [])
 .controller('SectionCtrl', function() {
-    this.sections = [
-        {
-            title : 'Electronics & Electrical',
+    
+    this.sections = [ {
+            title : 'Electronics & Electrical', image : "m1.jpg",
             items : [
                 { title : "Gaffar Market", image : "m1.jpg" },
                 { title : "Palika Bazaar", image : "m2.jpg" },
                 { title : "Lajpat Rai Market", image : "m3.jpg" }
             ]
         }, {
-            title : 'Fashion & Life Style',
+            title : 'Fashion & Life Style', image : "mk4.jpg",
             items : [
                 { title : "Sarojini Nagar", image : "mk4.jpg" },
                 { title : "Lajpat Nagar", image : "mk5.jpg" },
                 { title : "Karol Bagh", image : "mk6.jpg" }
             ]
         }, {
-            title : 'Food & Drink',
+            title : 'Food & Drink', image : "_mk7.jpg",
             items : [
                 { title : "Chandni Chowk", image : "_mk7.jpg" },
                 { title : "Rajouri Garden", image : "_mk8.jpg" },
                 { title : "Kamla Nagar", image : "_mk9.jpg" }
             ]
+        }
+    ];
+    
+})
+.controller('BannerCtrl', function() {
+    this.banners = [ {
+            title : 'Electronics & Electrical'
         }, {
-            title : 'Home Decor & Furniture',
-            items : [
-                { title : "Dilli Haat", image : "mk10.jpg" },
-                { title : "Paharganj", image : "mk11.jpg" },
-                { title : "Janpath market", image : "mk12.jpg" }
-            ]
+            title : 'Fashion & Life Style'
+        }, {
+            title : 'Food & Drink'
         }
     ];
     
